88715b51529c58c446899d2d2d4d21a2d9e70815
Alexandrbolshakov <alexandrbolshakov@yahoo.com>
2021-09-24 12:05:52 +0300

    first page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-    
+    $('.top-slider__inner').slick({
+        dots: true,
+        arrows: false,
+    })
+
+    const mixer1 = mixitup('.products__items');
+    const mixer2 = mixitup('.new__items');
 })
