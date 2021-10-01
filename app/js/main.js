14245c9a8159e82d11a95ced095ada3f550674e1
Alexandrbolshakov <alexandrbolshakov@yahoo.com>
2021-10-01 11:22:03 +0300

    add shop pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,33 @@
 $(function(){
+
+  $(".star").rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+
+  });
+  $(".star-product").rateYo({
+    starWidth: "16px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+
+  });
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function(data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function(data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
     $('.top-slider__inner').slick({
         dots: true,
         arrows: false,
